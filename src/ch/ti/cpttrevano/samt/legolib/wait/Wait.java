28542fdca4c3679f618afa8598c50bce5dd59f88
giuliobosco <giuliobva@gmail.com>
2018-12-14 16:08:05 +0100

    fix Wait - implemented class

--- src/ch/ti/cpttrevano/samt/legolib/wait/Wait.java
+++ src/ch/ti/cpttrevano/samt/legolib/wait/Wait.java
@@ -1,6 +1,34 @@
 
 package ch.ti.cpttrevano.samt.legolib;
 
-public class Wait {
-    
+/**
+ * Wait, used for generalization of all waiting class.
+ * In the Lego Mindstorm Environment is rappresented by the orange block "Wait".
+ *
+ * @author giuliobosco
+ * @version 1.0
+ */
+public class Wait extends Thread {
+
+    /**
+     * Empty constructor.
+     */
+    Wait() {}
+
+    /**
+     * Empty wait method.
+     */
+    public void wait() {}
+
+    /**
+     * Empty begin wait method.
+     */
+    public void beginWait() {}
+
+    /**
+     * Is finished method, returns always true.
+     */
+    public boolean isFinished() {
+        return true;
+    }
 }
