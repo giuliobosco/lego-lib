b4a9155894615fbd44cc95dc9cb6caeef4c1faa2
giuliobosco <giuliobva@gmail.com>
2019-01-18 15:56:56 +0100

    add PLFRunner (Proportional Line Follower Runner)

---
+++ src/ch/ti/cpttrevano/samt/legolib/examples/PLFRunner.java
@@ -0,0 +1,70 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2018 SAMT.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+ 
+package ch.ti.cpttrevano.samt.legolib.examples;
+
+import ch.ti.cpttrevano.samt.legolib.linefollower.ProportionalLineFollower;
+import ch.ti.cpttrevano.samt.legolib.motor.SingleMotor;
+import ch.ti.cpttrevano.samt.legolib.wait.WaitTime;
+import lejos.nxt.LightSensor;
+import lejos.nxt.SensorPort;
+
+/**
+ * Example of using proportional line follower.
+ *
+ * @author giuliobosco
+ * @version 1.0
+ */
+public class PLFRunner {
+    // ------------------------------------------------------------------------------------ Costants
+    // ---------------------------------------------------------------------------------- Attributes
+    // --------------------------------------------------------------------------- Getters & Setters
+    // -------------------------------------------------------------------------------- Constructors
+    // -------------------------------------------------------------------------------- Help Methods
+    // ----------------------------------------------------------------------------- General Methods
+    // --------------------------------------------------------------------------- Static Components
+
+    /**
+     * Main method of the class.
+     *
+     * @param args Command line arguments.
+     */
+    public static void main(String[] args) {
+        // Create proportional line follower
+        ProportionalLineFollower plf = new ProportionalLineFollower(
+                new SingleMotor('A'), // left motor on port A
+                new SingleMotor('B'), // right motor on port B
+                new LightSensor(SensorPort.S1), // frontal light sensor on port 1
+                new LightSensor(SensorPort.S2)); // back light sensor on port 2
+
+        // start the line follower
+        plf.start();
+
+        // wait 2 seconds
+        new WaitTime(2000).waiter();
+
+        // interrupt the proportional line follower
+        plf.interrupt();
+    }
+}
