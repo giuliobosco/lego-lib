224901185a0b0689ba72cd33b7c34bedba8381c1
giuliobosco <giuliobva@gmail.com>
2019-01-23 14:40:03 +0100

    add complie script

---
+++ src/compile.ps1
@@ -0,0 +1,9 @@
+# create the folder for the compiled classes
+mkdir ..\out\
+
+# compile all the classes
+nxjc .\ch\ti\cpttrevano\samt\legolib\wait\* -d ..\out\
+nxjc .\ch\ti\cpttrevano\samt\legolib\motor\* -d ..\out\
+nxjc .\ch\ti\cpttrevano\samt\legolib\linefollower\* -d ..\out\
+nxjc .\ch\ti\cpttrevano\samt\legolib\examples\* -d ..\out\
+nxjc .\ch\ti\cpttrevano\samt\legolib\test\* -d ..\out\
