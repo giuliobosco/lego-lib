e77b7e51249584caa19f5404f5518dccbd9a74aa
gabrialessi <gabrialessi23@gmail.com>
2019-01-26 21:10:55 +0100

    fix LineFollower - Javadoc

--- src/ch/ti/cpttrevano/samt/legolib/linefollower/LineFollower.java
+++ src/ch/ti/cpttrevano/samt/legolib/linefollower/LineFollower.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * Copyright 2018 SAMT.
+ * Copyright 2019 SAMT.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -29,40 +29,41 @@ import ch.ti.cpttrevano.samt.legolib.motor.SingleMotor;
 import ch.ti.cpttrevano.samt.legolib.wait.WaitLightSensor;
 
 /**
- * Line follower.
+ * LineFollower, uses a motor and a light sensor to follow a line.
  *
  * @author giuliobosco
- * @version 1.1
+ * @author gabrialessi
+ * @version 2.0
  */
 public class LineFollower extends Navigation {
-    // ------------------------------------------------------------------------------------ Costants
+    
+    // ------------------------------------------------------------------------- Constants
 
     /**
-     * Default value for line on the left.
-     * Value: true.
+     * Default value for line on the left (true).
      */
     public static final boolean DEFAULT_LINE_ON_LEFT = true;
 
-    // ---------------------------------------------------------------------------------- Attributes
+    // ------------------------------------------------------------------------- Fields
 
     /**
-     * Wait light sensor.
+     * The wait of the light sensor.
      */
     private WaitLightSensor waitLightSensor;
 
     /**
      * Line on the left.
-     * If true the line follower will keep the line on the left other ways it will
-     * keep the line on the right.
+     * If true the line follower will keep the line on the left otherwise it 
+     * will keep the line on the right.
      */
     private boolean lineOnLeft;
 
-    // --------------------------------------------------------------------------- Getters & Setters
+    // ------------------------------------------------------------------------- Getters
 
     /**
      * Set the the line on the left.
      *
-     * @param lineOnLeft Line on the left.
+     * @param lineOnLeft The line on the left.
      */
     public void setLineOnLeft(boolean lineOnLeft) {
         if (!this.isAlive()) {
@@ -79,31 +80,29 @@ public class LineFollower extends Navigation {
         return this.lineOnLeft;
     }
 
-    // -------------------------------------------------------------------------------- Constructors
+    // ------------------------------------------------------------------------- Constructors
 
     /**
-     * Create the line follower with the left motor, the right motor and the wait light sensor.
+     * Constructor method, creates the line follower with the left motor, the 
+     * right motor and the light sensor wait.
      *
-     * @param leftMotor       Left motor.
-     * @param rightMotor      Right motor.
-     * @param waitLightSensor Light sensor waiter.
+     * @param leftMotor The left motor.
+     * @param rightMotor The right motor.
+     * @param waitLightSensor The light sensor waiter.
      */
     public LineFollower(SingleMotor leftMotor, SingleMotor rightMotor, WaitLightSensor waitLightSensor) {
         super(leftMotor, rightMotor);
-
         this.lineOnLeft = DEFAULT_LINE_ON_LEFT;
     }
 
-    // -------------------------------------------------------------------------------- Help Methods
-    // ----------------------------------------------------------------------------- General Methods
+    // ------------------------------------------------------------------------- Help Methods
+    
+    // ------------------------------------------------------------------------- General Methods
 
-    /**
-     * Execute the line follower.
-     */
     @Override
     public void run() {
-        waitLightSensor.setValue((byte) 50);
-        waitLightSensor.setBigger(this.isLineOnLeft());
+        this.waitLightSensor.setCheckValue((byte) 50);
+        this.waitLightSensor.setBigger(this.isLineOnLeft());
 
         this.startNavigation();
         while (!isAlive()) {
@@ -126,8 +125,5 @@ public class LineFollower extends Navigation {
             waitLightSensor.setBigger(!waitLightSensor.isBigger());
         }
     }
-
-
-    // --------------------------------------------------------------------------- Static Components
 
 }
