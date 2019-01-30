fc4181c5f5d83b1c39fb1970a10972a1ea7c9472
gabrialessi <gabrialessi23@gmail.com>
2019-01-30 16:25:12 +0100

    fix compile.ps1 - UseWaitTimeSynchron test

--- src/compile.ps1
+++ src/compile.ps1
@@ -3,3 +3,9 @@ mkdir .\out\
 
 # compile all the classes
 nxjc .\*.java -d .\out\
+
+# switch to the compiled classes directory
+cd out
+
+# link the compiled classes to a nxj file
+nxjlink -o .\Test.nxj UseWaitTimeSynchron
