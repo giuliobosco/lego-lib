580f6604abe3143d181e4c85bed10e8b21af022d
gabrialessi <gabrialessi23@gmail.com>
2019-01-30 14:40:28 +0100

    fix compile.ps1 - directory

--- src/compile.ps1
+++ src/compile.ps1
@@ -1,9 +1,5 @@
 # create the folder for the compiled classes
-mkdir ..\out\
+mkdir .\out\
 
 # compile all the classes
-nxjc .\ch\ti\cpttrevano\samt\legolib\wait\* -d ..\out\
-nxjc .\ch\ti\cpttrevano\samt\legolib\motor\* -d ..\out\
-nxjc .\ch\ti\cpttrevano\samt\legolib\linefollower\* -d ..\out\
-nxjc .\ch\ti\cpttrevano\samt\legolib\examples\* -d ..\out\
-nxjc .\ch\ti\cpttrevano\samt\legolib\test\* -d ..\out\
+nxjc .\*.java -d .\out\
