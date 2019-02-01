c6bd31ee5c408e2aab6b325c313fbeec2110be5e
gabrialessi <gabrialessi23@gmail.com>
2019-02-01 16:25:55 +0100

    fix compile.ps1 - WaitTime

--- src/compile.ps1
+++ src/compile.ps1
@@ -2,10 +2,14 @@
 mkdir .\out\
 
 # compile all the classes
-nxjc .\*.java -d .\out\
+nxjc .\WaitTime.java -d .\out\
+nxjc .\UseWaitTime.java -d .\out\
+
 
 # switch to the compiled classes directory
 cd out
 
 # link the compiled classes to a nxj file
-nxjlink -o .\Test.nxj UseWaitTimeSynchron
+nxjlink -o .\Test.nxj UseWaitTime
+
+nxjupload -r Test.nxj
