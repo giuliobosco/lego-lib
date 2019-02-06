0853dd2c5301c02b7435054f481925a5db06190c
gabrialessi <gabrialessi23@gmail.com>
2019-02-06 13:41:58 +0100

    add LineFollower test

--- src/compile.ps1
+++ src/compile.ps1
@@ -2,14 +2,16 @@
 mkdir .\out\
 
 # compile all the classes
-nxjc .\WaitTime.java -d .\out\
-nxjc .\UseWaitTime.java -d .\out\
+nxjc .\WaitLightSensor.java -d .\out\
+nxjc .\SingleMotor.java -d .\out\
+nxjc .\LineFollower.java -d .\out\
+nxjc .\LFRunner.java -d .\out\
 
 
 # switch to the compiled classes directory
 cd out
 
 # link the compiled classes to a nxj file
-nxjlink -o .\Test.nxj UseWaitTime
+nxjlink -o .\Test.nxj LFRunner
 
 nxjupload -r Test.nxj
