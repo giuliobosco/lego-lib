91cd87bb8ba789b1e10d9f0e668fba0a51db5836
gabrialessi <gabrialessi23@gmail.com>
2019-02-07 18:39:16 +0100

    fix HelloWorld - Javadoc

--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -1,8 +1,24 @@
+package test;
+
 import lejos.nxt.Button;
 
+/**
+ * Test class. Prints "Hello World" on the brick.
+ *
+ * @author gabrialessi
+ * @version 1.0 (2019-02-07)
+ */
 public class HelloWorld {
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-		Button.waitForAnyPress();
-	}
+
+    /**
+     * Main method.
+     *
+     * @param args Command line arguments.
+     */
+    public static void main(String[] args) {
+        // Print "Hello World".
+        System.out.println("Hello World");
+        // Wait for a button to end the test.
+        Button.waitForAnyPress();
+    }
 }
